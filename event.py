6b087a8feda30aab560c81b94c9e3d9815ae27bc
Nishant Maurya <47202762+nishantmaurya1@users.noreply.github.com>
2019-03-26 15:54:30 +0530

    Update event.py

--- event.py
+++ event.py
@@ -8,7 +8,7 @@ execution_path = os.getcwd()
 prediction = CustomImagePrediction()
 prediction.setModelTypeAsResNet()
 prediction.setModelPath("idenprof_061-0.7933.h5")
-prediction.setJsonPath("idenprof_model_class.json")
+prediction.setJsonPath("model_class.json")
 prediction.loadModel(num_objects=10)
 
 def predictor(file):
